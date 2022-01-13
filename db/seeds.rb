23ceeb1994bbbb7771e327782f0f7d4266cb6844
Saurabh Shinde <saurabhshinde23@gmail.com>
2022-01-13 17:41:26 +0530

    added seeds data for testing purpose

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Post
+post = Post.create(title: 'New Post', body: 'This is a new post')
+
+# Create top level comments
+25.times do |i|
+  comment = Comment.create(body: "This is #{i + 1} comment", post: post)
+  # Create replies(second level) for top level comments
+  5.times do
+    reply = Comment.create(body: "This is reply of Comment #{comment.id}", post: post, parent: comment)
+    # Create replies for second level comments
+    3.times do
+      Comment.create(body: "This is reply of Comment #{reply.id}", post: post, parent: reply)
+    end
+  end
+end
